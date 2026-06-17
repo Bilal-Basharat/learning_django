e308eea01f42842c5d412cb4de0e92b55049131e
Bilal Basharat <bilalbisharat@gmail.com>
2026-06-17 12:35:35 +0500

    chore(db): explore migrations, models, and MySQL setup

--- firstapp/models.py
+++ firstapp/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here.
+class student(models.Model):
+    name = models.CharField(max_length=100)
+    age = models.IntegerField()
+    email = models.EmailField(null=True, blank=True)
+    address = models.TextField(null=True, blank=True)
+    image = models.ImageField(upload_to='images/')
+    file = models.FileField(upload_to='files/', null=True, blank=True)
+
+class teacher(models.Model):
+    pass
+    
+class Reservation(models.Model):
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    guest_count = models.IntegerField()
+    reservation_time = models.DateField(auto_now=True)
+    comments = models.TextField(null=True, blank=True)
+    
+    def __str__(self):
+        return f"{self.first_name} {self.last_name} - {self.reservation_time}"
